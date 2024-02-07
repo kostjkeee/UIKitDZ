4dbea796f0613bcb096aed92ba1cc47f5f293348
kostjkeee <darkveider_77@mail.ru>
2024-02-07 23:54:54 +1000

    добавил swiftlint, swiftformatter и устранил ошибки

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,12 @@
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// main view controller
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi")
+    }
+}
