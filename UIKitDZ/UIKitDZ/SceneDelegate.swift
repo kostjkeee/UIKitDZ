4e3557c02378bb7e8fae999419618e709cf57488
kostjkeee <darkveider_77@mail.ru>
2024-02-13 00:22:55 +1000

    создал шаблон без storyboard

--- UIKitDZ/UIKitDZ/SceneDelegate.swift
+++ UIKitDZ/UIKitDZ/SceneDelegate.swift
@@ -7,5 +7,13 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {}
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
 }
